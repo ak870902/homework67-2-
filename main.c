da952557e657a200cf7018ae976fdf387357d111
Your Nameak870902 <ak870902@gmail.com>
2018-05-28 18:41:41 +0800

    a non-constant pointer to constant data

--- main.c
+++ main.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-void S0 (int *data);
-void S1 ( int *const data1);
+void S0 (const int *data);
+void S1 (const int * data1);
 
 
 int main(void)
 {
+
 	int d[10];
 	S0(d);
 	S1(d);
